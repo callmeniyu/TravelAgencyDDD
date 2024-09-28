7f680f01c7ef0471a82202a3ec437dabcb8bef2d
callmeniyu <getniyashere@gmail.com>
2024-09-28 22:59:46 +0530

    loader

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react"
 import "./Home.css"
-import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from "@ap.cx/react-fullpage";
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from "@ap.cx/react-fullpage"
 const Hero = React.lazy(() => import("../../components/Hero/Hero"))
 const About = React.lazy(() => import("../../components/About/About"))
 const Pricing = React.lazy(() => import("../../components/Pricing/Pricing"))
@@ -13,38 +13,37 @@ import PropagateLoader from "react-spinners/PropagateLoader"
 const Home = () => {
     return (
         <div className="home">
+            <Suspense
+                fallback={
+                    <div className="loader-container">
+                        <PropagateLoader data-testid="loader" color="#f1693b" className="loader" />
+                    </div>
+                }
+            ></Suspense>
             <Fullpage>
                 <FullpageNavigation style={{ cursor: "pointer", width: "1rem" }} />
                 <FullPageSections>
-                    <Suspense
-                        fallback={
-                            <div className="loader-container">
-                                <PropagateLoader data-testid="loader" color="#f1693b" className="loader" />
-                            </div>
-                        }
-                    >
-                        <FullpageSection>
-                            <Hero id="hero" />
-                        </FullpageSection>
-                        <FullpageSection>
-                            <About id="about" />
-                        </FullpageSection>
-                        <FullpageSection>
-                            <Destinations id="destinations" />
-                        </FullpageSection>
-                        <FullpageSection>
-                            <Pricing id="pricing" />
-                        </FullpageSection>
-                        <FullpageSection>
-                            <Testimonials id="testimonials" />
-                        </FullpageSection>
-                        <FullpageSection>
-                            <Contact id="contact" />
-                        </FullpageSection>
-                        <FullpageSection style={{ height: "60vh" }}>
-                            <Footer />
-                        </FullpageSection>
-                    </Suspense>
+                    <FullpageSection>
+                        <Hero id="hero" />
+                    </FullpageSection>
+                    <FullpageSection>
+                        <About id="about" />
+                    </FullpageSection>
+                    <FullpageSection>
+                        <Destinations id="destinations" />
+                    </FullpageSection>
+                    <FullpageSection>
+                        <Pricing id="pricing" />
+                    </FullpageSection>
+                    <FullpageSection>
+                        <Testimonials id="testimonials" />
+                    </FullpageSection>
+                    <FullpageSection>
+                        <Contact id="contact" />
+                    </FullpageSection>
+                    <FullpageSection style={{ height: "60vh" }}>
+                        <Footer />
+                    </FullpageSection>
                 </FullPageSections>
             </Fullpage>
         </div>
